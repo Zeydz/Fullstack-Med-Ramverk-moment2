b1d9399924e510a683ba2849bf09479d1f25af6f
Jocke <joakim03moller@gmail.com>
2025-11-19 13:37:36 +0100

    Lagt till schema i tracks.module

--- src/tracks/tracks.module.ts
+++ src/tracks/tracks.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TracksService } from './tracks.service';
 import { TracksController } from './tracks.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Track, TrackSchema } from './schemas/track.schema';
 
 @Module({
+  // Registrerar Track-schemat i Mongoose
+  imports: [
+    MongooseModule.forFeature([{ name: Track.name, schema: TrackSchema }])
+  ],
   controllers: [TracksController],
   providers: [TracksService],
 })

fa9da7755fc085ac3c63f1a958c3b7b6f88c830f
Jocke <joakim03moller@gmail.com>
2025-11-19 11:38:33 +0100

    Skapat tracks resource med controller, module, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TracksModule } from './tracks/tracks.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [TracksModule],
 })
 export class AppModule {}

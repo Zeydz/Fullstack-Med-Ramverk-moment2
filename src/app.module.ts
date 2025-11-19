299725eb920826f9d99432813f9a38f69d745982
Jocke <joakim03moller@gmail.com>
2025-11-19 12:48:11 +0100

    Lagt till .env-fil för databasanslutning

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TracksModule } from './tracks/tracks.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TracksModule } from './tracks/tracks.module';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // läser .env
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'), // hämtar URL från .env
+      }),
+    }),
+    TracksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
-  imports: [TracksModule],
 })
 export class AppModule {}
